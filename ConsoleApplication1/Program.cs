42ddce121c756215d900f4ad4e78d63fc942c8b9
dhultengren <david@statdna.com>
2017-04-01 16:18:33 -0400

    Working header ingestion

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:8011/api/Dipper");
+            client.DefaultRequestHeaders.Add("action", "GetCheckins");
+
+            HttpResponseMessage response = client.GetAsync("").Result;
+            var results = response.Content.ReadAsStringAsync();
+
+            string wtf = "wtf";
+        }
+    }
+}
---
+++ DipperRestService/Controllers/DipperController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace DipperRestService.Controllers
+{
+    public class DipperController : ApiController
+    {
+        private string FolderPath = string.Empty;
+        private string CheckinFilepath = string.Empty;
+
+        public string[] Get()
+        {
+            var ctx = HttpContext.Current;
+            string action = ctx.Request.Headers["action"];
+            
+            if (string.IsNullOrEmpty(FolderPath))
+            {
+                ReadConfigValues();
+            }
+
+            if (action == "GetCheckins")
+            {
+                using (StreamReader sr = new StreamReader(FolderPath + CheckinFilepath))
+                {
+                    return sr.ReadLine().Split(',');
+                }
+            }
+
+            return new string[] {"There are real results returned from the service, seriously"};
+        }
+
+        private void ReadConfigValues()
+        {
+            FolderPath = ConfigurationManager.AppSettings["FolderPath"];
+            CheckinFilepath = ConfigurationManager.AppSettings["CheckinFilepath"];
+        }
+    }
+}
